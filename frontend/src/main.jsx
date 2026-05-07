89ca48c2047e87124d893fbb16298900e3e88aa8
Akash <akashbawanthade02@gmail.com>
2026-05-08 01:52:06 +0530

    feat: add Vercel Analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import './index.css'
 import { StrictMode } from "react";
 import store from './app/store.js'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
-
-
-
-const  PUBLISHABLE_KEY  = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key. Add VITE_CLERK_PUBLISHABLE_KEY to your .env file.");
@@ -20,11 +18,12 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-       <Provider store={store}>
-         <Router>
-           <App />
-         </Router>
-       </Provider>
+      <Provider store={store}>
+        <Router>
+          <App />
+          <Analytics />
+        </Router>
+      </Provider>
     </ClerkProvider>
   </StrictMode>
 );
